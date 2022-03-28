045d89fb2ff1fab34f9711a161f42cd43e600c4f
Fabián Esqueda <fabian.esqueda2892@alumnos.udg.mx>
2022-03-28 14:18:23 -0600

    Se cambio la ruta en el servidor

---
+++ server.js
@@ -0,0 +1,13 @@
+/*Crear nuestro servidor*/
+const express = require('express');
+const path = require('path'); 
+
+const app = express(); 
+
+app.use(express.static('./dist/angular-app'));
+
+app.get('/*', (req,res) =>
+        res.sendFile('index.html', {root: 'dist/angular-app/'}),
+    );
+
+app.listen(process.env.PORT || 8080);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'TO-DO LIST';
+  list:any[] = [];//La lista va a poder recibir cualquier cosa y por defaulta estaá en blanco
+  addTask(item: string) { //Funcion que se llama desde app.component.html
+      this.list.push({
+      id:this.list.length, name:item
+    });
+    console.warn(this.list);
+  }
+
+  removeTask(id:number) {
+    console.warn(id);
+    this.list = this.list.filter(item => item.id !== id); 
+  }
 }
